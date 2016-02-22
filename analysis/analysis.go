489d1c53a0fc874407b71c55964c60b725176503
Jose Geraldo de Carvalho Pereira <jgcarvalho@usp.br>
2016-02-22 03:22:56 -0300

    alpha version

--- analysis/analysis.go
+++ analysis/analysis.go
@@ -7,6 +7,50 @@ import (
 	"github.com/jgcarvalho/toniHB/pdb"
 )
 
+func isAcceptor(atm pdb.Atom) bool {
+	switch atm.Name {
+	// CHARMM27 and OPLS/AA protein HB acceptors
+	case "O", "OD1", "OD2", "OE1", "OE2", "OG1", "OH":
+		// ASPP and GLUP are protonated so they don't have acceptors
+		if atm.ResName == "ASPP" || atm.ResName == "GLUP" {
+			return false
+		}
+		return true
+	case "ND1", "NE2":
+		// HSD, HSE and HSP are used to indicate which atom is protonated
+		if atm.ResName == "HSD" && atm.Name == "NE2" {
+			return true
+		} else if atm.ResName == "HSE" && atm.Name == "ND1" {
+			return true
+		} else if atm.ResName == "HSP" {
+			return false
+			// HIS don't indicate which atom is protonated
+		} else if atm.ResName == "HIS" {
+			return true
+		} else {
+			return false
+		}
+	default:
+		return false
+	}
+}
+
+func hbangle(Namd [3]float64, Hamd [3]float64, atm [3]float64) float64 {
+	// v1 = vector H->N and v2 = vector H->acceptor
+	v1 := [3]float64{Namd[0] - Hamd[0], Namd[1] - Hamd[1], Namd[2] - Hamd[2]}
+	v2 := [3]float64{atm[0] - Hamd[0], atm[1] - Hamd[1], atm[2] - Hamd[2]}
+
+	v1mag := math.Sqrt(v1[0]*v1[0] + v1[1]*v1[1] + v1[2]*v1[2])
+	v1norm := [3]float64{v1[0] / v1mag, v1[1] / v1mag, v1[2] / v1mag}
+
+	v2mag := math.Sqrt(v2[0]*v2[0] + v2[1]*v2[1] + v2[2]*v2[2])
+	v2norm := [3]float64{v2[0] / v2mag, v2[1] / v2mag, v2[2] / v2mag}
+
+	res := v1norm[0]*v2norm[0] + v1norm[1]*v2norm[1] + v1norm[2]*v2norm[2]
+	angle := math.Pi - math.Acos(res)
+	return angle
+}
+
 func distance(amd [3]float64, atm [3]float64) float64 {
 	sqX := math.Pow(amd[0]-atm[0], 2)
 	sqY := math.Pow(amd[1]-atm[1], 2)
@@ -14,41 +58,52 @@ func distance(amd [3]float64, atm [3]float64) float64 {
 	return math.Sqrt(sqX + sqY + sqZ)
 }
 
-// TODO should contact consider valid atom types?
-// Contacts are calculated with H atom (from NH) as sphere center
+// Contacts are calculated with N atom (from NH) as sphere center
 func inContact(amd pdb.Amide, atm pdb.Atom, dist float64) bool {
-	return distance(amd.Hxyz, atm.XYZ) < dist
+	return distance(amd.Nxyz, atm.XYZ) < dist
 }
 
-// TODO
-func OdoHB(amd pdb.Amide, atm pdb.Atom, dist float64, angle float64) bool {
-	return false
-}
-
-// TODO
-func NdoHB(amd pdb.Amide, atm pdb.Atom, dist float64, angle float64) bool {
+// Tests if there are a HB
+func doHB(amd pdb.Amide, atm pdb.Atom, dist float64, angle float64) bool {
+	// test if atom is an acceptor
+	if isAcceptor(atm) {
+		// test if distance H to acceptor is between 1.5 and dist (user defined)
+		if distance(amd.Hxyz, atm.XYZ) > 1.5 && distance(amd.Hxyz, atm.XYZ) < dist {
+			// test if angle is smaller than angle (user defined)
+			if hbangle(amd.Nxyz, amd.Hxyz, atm.XYZ) < angle {
+				return true
+			}
+		}
+	}
+	// if some tests above fails
 	return false
 }
 
 func Run(amd []pdb.Amide, atm []pdb.Atom, validAtoms string, maxDist float64, hbDist float64, hbAngle float64) {
 	for i := 0; i < len(amd); i++ {
 		for j := 0; j < len(atm); j++ {
-			// skip if atomtype is not in the user input
+			// skip if atomtype is not valid (user input)
 			if strings.Contains(validAtoms, atm[j].Type) {
 				// skip if is the same atom
 				if amd[i].PDBNumber != atm[j].PDBNumber {
-					// TODO should contact consider valid atom types?
 					if inContact(amd[i], atm[j], maxDist) {
 						amd[i].NumContacts++
 					}
-					if OdoHB(amd[i], atm[j], hbDist, hbAngle) {
-						amd[i].NumOhb++
+					if atm[j].Type == "O" {
+						if doHB(amd[i], atm[j], hbDist, hbAngle) {
+							amd[i].NumOhb++
+						}
 					}
-					if NdoHB(amd[i], atm[j], hbDist, hbAngle) {
-						amd[i].NumNhb++
+					if atm[j].Type == "N" {
+						if doHB(amd[i], atm[j], hbDist, hbAngle) {
+							amd[i].NumNhb++
+						}
 					}
 				}
 			}
+		}
+		if amd[i].NumOhb > 0 || amd[i].NumNhb > 0 {
+			amd[i].DoHB = true
 		}
 	}
 }
--- toniHB.go
+++ toniHB.go
@@ -1,23 +1,120 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/jgcarvalho/toniHB/analysis"
 
 	"github.com/jgcarvalho/toniHB/pdb"
 )
 
+// Analyse all pdb files
+func Analyse(dir, validAtoms string, radius, dist, angle float64, output string) {
+	absDir, err := filepath.Abs(dir)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+
+	pdbFiles, err := filepath.Glob(absDir + "/*")
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+	if len(pdbFiles) == 0 {
+		log.Println("ERROR: PDB files not found in path", absDir)
+	}
+	fmt.Printf("%d PDB files founded at %s\n", len(pdbFiles), absDir)
+
+	allPDBamides := make([][]pdb.Amide, len(pdbFiles))
+	allPDBatoms := make([][]pdb.Atom, len(pdbFiles))
+	for i := 0; i < len(pdbFiles); i++ {
+		allPDBamides[i], allPDBatoms[i], err = pdb.LoadFile(pdbFiles[i])
+		analysis.Run(allPDBamides[i], allPDBatoms[i], validAtoms, radius, dist, angle)
+	}
+	// write result to file
+	Report(allPDBamides, output)
+	fmt.Println("DONE")
+}
+
+// Write result to output file
+func Report(allPDBamides [][]pdb.Amide, output string) {
+	f, err := os.Create(output)
+	if err != nil {
+		log.Println("ERROR:", err)
+		return
+	}
+	defer f.Close()
+
+	nPDB := len(allPDBamides)
+	nAmides := len(allPDBamides[0])
+	// print header
+	fmt.Fprint(f, "Number, Residue")
+	for i := 0; i < nPDB; i++ {
+		fmt.Fprintf(f, ", Contacts_%d, O_%d, N_%d, H-bond_%d", i+1, i+1, i+1, i+1)
+	}
+	fmt.Fprint(f, "\n")
+	// print data
+	for i := 0; i < nAmides; i++ {
+		for j := 0; j < nPDB; j++ {
+			if j == 0 {
+				fmt.Fprintf(f, "%d, \"%s %s\"", allPDBamides[j][i].Number, allPDBamides[j][i].ResName, allPDBamides[j][i].ResNumber)
+			}
+			fmt.Fprintf(f, ", %d, %d, %d, %t", allPDBamides[j][i].NumContacts, allPDBamides[j][i].NumOhb, allPDBamides[j][i].NumNhb, allPDBamides[j][i].DoHB)
+			if j == (nPDB - 1) {
+				fmt.Fprintf(f, "\n")
+			}
+		}
+	}
+}
+
 func main() {
 	// parse parameters
-	amd, atm, err := pdb.LoadFile("./test/lysozime/em.pdb")
-	// calculate
-	analysis.Run(amd, atm, "CONPH", 3.6, 3.6, 0.5)
 
-	fmt.Println("ATOMS")
-	fmt.Println(atm)
-	fmt.Println("AMIDES")
-	fmt.Println(amd)
+	validAtoms := flag.String("type", "", "Valid atom types. Ex. -type CNOPH or -type \"C N O P H\"")
+	radius := flag.Float64("radius", 0.0, "Radius of the sphere to count atoms around N atom")
+	dist := flag.Float64("dist", 0.0, "Maximum distance between H and acceptor")
+	angle := flag.Float64("angle", -1.0, "Maximum angle between vectors H->N and H->acceptor (radians)")
+	dir := flag.String("dir", "", "PDB files directory (the files should be named like somename_0001.pdb ... somename_9999.pdb)")
+	output := flag.String("o", "", "Output file")
+	flag.Parse()
 
-	fmt.Println(err)
+	if *dir == "" {
+		fmt.Println("Please, set the pdb files path using the flag -dir")
+		return
+	}
+	if *validAtoms == "" {
+		fmt.Println("Please, set the valid atom types using the flag -type")
+		return
+	}
+	if *radius == 0.0 {
+		fmt.Println("Please, set the radius of the sphere using the flag -radius")
+		return
+	}
+	if *dist == 0.0 {
+		fmt.Println("Please, set the maximum HB distance using the flag -dist")
+		return
+	}
+	if *angle == -1.0 {
+		fmt.Println("Please, set the maximum HB angle using the flag -angle")
+		return
+	}
+	if *output == "" {
+		fmt.Println("Please, set the output file using the flag -o")
+		return
+	}
+
+	fmt.Println("PDB path:", *dir)
+	fmt.Println("Valid atoms:", *validAtoms)
+	fmt.Println("Radius of the sphere to count atoms around N:", *radius)
+	fmt.Println("Maximum distance between H and accepto:", *dist)
+	fmt.Println("Maximum angle between vectors H->N and H->acceptor (radians):", *angle)
+
+	fmt.Println("\nRunning...")
+
+	Analyse(*dir, *validAtoms, *radius, *dist, *angle, *output)
 }
